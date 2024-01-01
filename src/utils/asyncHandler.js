356c8b4b4649da0f394921e810ba80ce352736a6
deep1296 <deeptange@gmail.com>
2024-01-01 22:22:16 +0530

    Some changes done - Resolved some errors

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,9 +2,11 @@
 
 const asyncHandler = (requestHandler) => {
   return (req, res, next) => {
-    return Promise.resolve(requestHandler(req, res, next)).catch((err) =>
-      next(err)
-    );
+    try {
+       Promise.resolve(requestHandler(req, res, next));
+    } catch (err) {
+      return next(err);
+    }
   };
 };
 
